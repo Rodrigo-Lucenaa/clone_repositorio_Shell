bd1a3ecb53fc50bc7053ad42d65f5ad798110089
Rodrigo <r.rodrigo523@gmail.com>
2022-03-13 22:56:23 -0300

    Update clone_repositorio.sh

--- clone_repositorio.sh
+++ clone_repositorio.sh
@@ -2,7 +2,7 @@
 
 mkdir repositorios-git
 cd repositorios-git
-echo "Digite nome do usuário GITHUB"
+echo "Digite nome do usuário GitHub"
 read nome
 
 repos=$(curl -s https://api.github.com/users/$nome/repos | awk '/clone_url/{print $2}' | sed 's/^"//g' | sed 's/",$//g')
