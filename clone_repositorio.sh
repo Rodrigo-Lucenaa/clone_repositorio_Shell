6fc40fb0f7b476cbca078081c7fdbdc79903d8b6
Rodrigo <r.rodrigo523@gmail.com>
2022-07-04 18:50:34 -0300

    Update clone_repositorio.sh

--- clone_repositorio.sh
+++ clone_repositorio.sh
@@ -5,6 +5,8 @@ cd repositorios-git
 echo "Digite nome do usuário GitHub"
 read nome
 
+apt install git -y && apt install curl -y
+
 repos=$(curl -s https://api.github.com/users/$nome/repos | awk '/clone_url/{print $2}' | sed 's/^"//g' | sed 's/",$//g')
 
 for repos in $repos
